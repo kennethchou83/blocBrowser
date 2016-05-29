14b702675080f567209e6680dd22e495ca979ee9
Kenneth Chou <kennethchou83@yahoo.com>
2016-05-29 14:13:18 -0700

    chapter 24 message

---
+++ blocbroswer_chapter_24/BlocBroswer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBroswer
+//
+//  Created by Kenneth Chou on 5/25/16.
+//  Copyright © 2016 Kenneth Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ blocbroswer_chapter_24/BlocBroswer/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  BlocBroswer
+//
+//  Created by Kenneth Chou on 5/25/16.
+//  Copyright © 2016 Kenneth Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+//Replaces the web view with a fresh one, erasing all history. Also updates the URL field and toolbar buttons appropriately.
+- (void) resetWebView;
+
+
+@end
+
